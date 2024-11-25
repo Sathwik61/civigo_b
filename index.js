6bc7d7f1a14b0daf572662077639097a57b1f645
SATHWIK <sathwikacharya61@gmail.com>
2024-11-25 21:04:26 +0530

    t

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors("*"));
-app.use('/',(req,res)=>{return res.json({message:"Welcome to the entry"})});
+app.get('/',(req,res)=>{return res.json({message:"Welcome to the entry"})});
 app.use('/api/auth', authRoutes);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB\n'))
